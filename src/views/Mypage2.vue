179be9e792e9f06995cc263cb21af87c7613de8f
Dio <91955876+JangG1@users.noreply.github.com>
2021-12-30 19:20:45 +0900

    Update Mypage2.vue

--- src/views/Mypage2.vue
+++ src/views/Mypage2.vue
@@ -1,22 +1,22 @@
 <template>
 <div>
     <!--top-->
-  <div class="top" style="text-align:center">
-           <router-link v-bind:to="'/'"> <img src="@/assets/logo.jpg" alt="logo"></router-link>
+    <div class="top" style="text-align:center">
+        <router-link v-bind:to="'/'"> <img src="@/assets/logo.jpg" alt="logo"></router-link>
         <h1>마이페이지</h1>
     </div>
     <!--left-->
     <div class="container">
         <div class="left">
-         <div class="List">
-                   <router-link v-bind:to="'/mypage1'">나의쇼핑내역</router-link>
+            <div class="List">
+                <router-link v-bind:to="'/mypage1'">나의쇼핑내역</router-link>
             </div>
             <div class="check">
                 <li>주문/배송 조회</li>
                 <li>취소/환불 조회</li>
             </div>
-              <div class="List">
-                  <router-link v-bind:to="'/mypage2'">회원정보 열람</router-link>
+            <div class="List">
+                <router-link v-bind:to="'/mypage2'">회원정보 열람</router-link>
             </div>
             <div class="check">
                 <li>회원정보 수정</li>
@@ -31,43 +31,43 @@
             <div class="content">
                 <h3>
                     닉네임
-                <input type="text" class="mdText" id="m1" v-model="putnick">
-                <button class="mdBtn" @click="nickMod"> 수정하기 </button>
+                    <input type="text" class="mdText" id="m1" v-model="putnick">
+                    <button class="mdBtn" @click="nickMod"> 수정하기 </button>
                 </h3>
             </div>
             <div class="content">
                 <h3>
                     비밀번호
-                <input  v-model="signup.password" type="password" class="mdText" id="m2" @blur="passwordValid">
-                 <button class="mdBtn" > 수정하기 </button>
-                 <div v-if="!passwordValidFlag" class="pwFlag"> 유효하지 않은 비밀번호 입니다. </div>
+                    <input v-model="signup.password" type="password" class="mdText" id="m2" @blur="passwordValid">
+                    <button class="mdBtn" @click="pwMod"> 수정하기 </button>
+                    <div v-if="!passwordValidFlag" class="pwFlag"> 유효하지 않은 비밀번호 입니다. </div>
                 </h3>
                 <br>
             </div>
-    
+
             <div class="content">
                 <h3>
-                    연락처
-                <input type="text" class="mdText" id="m3" v-model="putNum" placeholder="- 없이 숫자만" maxlength="11">
-                <button class="mdBtn" @click="numMod"> 수정하기 </button>
+                    전화번호
+                    <input  v-model="putNum" type="text" class="mdText" id="m3" @blur="numValid" placeholder="- 없이 숫자만" maxlength="11">
+                    <button class="mdBtn" @click="numMod"> 수정하기 </button>
+                    <div v-if="!numValidFlag" class="numFlag"> 유효하지 않은 전화번호 입니다. </div>
                 </h3>
             </div>
             <div class="content">
                 <h3>
-                    주소 
-                <input type="text" class="mdText" id="m4">
-                <button class="mdBtn"> 주소조회 </button>
+                    주소
+                    <input type="text" class="mdText" id="m4">
+                    <button class="mdBtn"> 주소조회 </button>
                 </h3>
             </div>
             <div class="content">
                 <h3>
                     <label>상세주소</label>
-                <input type="text" class="mdText" id="m5">
+                    <input type="text" class="mdText" id="m5">
                 </h3>
             </div>
             <div class="content">
-                <button class="mod">수정하기</button>
-                <router-link v-bind:to="'/'"> <button class="quit">회원탈퇴</button></router-link>
+                <router-link v-bind:to="'/'"> <button class="quit" @click="quitBtn">회원탈퇴</button></router-link>
             </div>
         </div>
     </div>
@@ -78,14 +78,16 @@
 export default {
     data() {
         return {
-            signup: { 
-            password: null, 
-            pwhint: '', 
-            pwhintans: null 
-            }, 
-            passwordValidFlag: true, 
-            passwordCheck: '', 
+            signup: {
+                password: null,
+                pwhint: '',
+                pwhintans: null,
+                putNum: null,
+            },
+            passwordValidFlag: true,
+            passwordCheck: '',
             passwordCheckFlag: true,
+            numValidFlag: true,
             putNum: '',
             putnick: '',
             msg: '',
@@ -96,35 +98,62 @@ export default {
             try {
                 if ("" === this.putnick) {
                     alert("공백 입니다.");
-                }else if("asd" === this.putnick){
+                } else if ("asd" === this.putnick) {
                     alert("이미 가입된 닉네임 입니다.");
-                    } else  {
-                        alert("수정 되었습니다.");
-                    }   
+                } else {
+                    alert("수정 되었습니다.");
+                }
             } catch (err) {
                 this.msg = "error";
             }
         },
-        passwordValid () { 
-            if (/^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9]).{8,16}$/.test(this.signup.password)) { 
-                this.passwordValidFlag = true 
-        } else { this.passwordValidFlag = false 
-        }
+        passwordValid() {
+            if (/^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9]).{8,16}$/.test(this.signup.password)) {
+                this.passwordValidFlag = true
+            } else {
+                this.passwordValidFlag = false
+            }
 
         },
-        numMod(){
+        pwMod() {
             try {
-                 if ("" === this.putNum) {
+                if ("" === this.signup.password) {
                     alert("공백 입니다.");
-                }else if(/^(01[016789]{1}|02|0[3-9]{1}[0-9]{1})-?[0-9]{3,4}-?[0-9]{4}$/.test(this.putNum)) {
-                    alert("수정 되었습니다.");
-                }else{
-                    alert("제대로 입력해주세요.")
-                }   
+                } else if (/^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9]).{8,16}$/.test(this.signup.password)){
+                    if(this.passwordValidFlag == true)
+                    alert("수정되었습니다.");
+                } else {
+                    if(!this.passwordValidFlag == true)
+                    alert("비밀번호 형식은 대문자,소문자,숫자 포함 8~16글자 입니다.");
+                }
             } catch (err) {
                 this.msg = "error";
             }
         },
+        numValid() {
+            if (/^(01[016789]{1}|02|0[3-9]{1}[0-9]{1})-?[0-9]{3,4}-?[0-9]{4}$/.test(this.putNum)) {
+                this.numValidFlag = true
+            } else {
+                this.numValidFlag = false
+            }
+
+        },
+        numMod() {
+            try {
+                if ("" === this.putNum) {
+                    alert("공백 입니다.");
+                } else if (/^(01[016789]{1}|02|0[3-9]{1}[0-9]{1})-?[0-9]{3,4}-?[0-9]{4}$/.test(this.putNum)) {
+                    alert("수정 되었습니다.");
+                } else {
+                    alert("제대로 입력해주세요.")
+                }
+            } catch (err) {
+                this.msg = "error";
+            }
+        },
+        quitBtn(){
+            alert("정말 탈퇴하시겠습니까?")
+        }
     }
 }
 </script>
@@ -133,7 +162,6 @@ export default {
 button {
     font-family: 맑은고딕, Malgun Gothic, dotum, gulim, sans-serif;
     background-color: #0051ba;
-    /* background-color: #fafafa; */
     border-radius: 4px;
     border: none;
     color: white;
@@ -145,25 +173,34 @@ button {
     cursor: pointer;
     margin: 0px 1px;
 }
-h1{
+
+h1 {
     font-family: 맑은고딕, Malgun Gothic, dotum, gulim, sans-serif;
     font-size: 32px;
 }
-h2{
+
+h2 {
     font-family: 맑은고딕, Malgun Gothic, dotum, gulim, sans-serif;
     font-size: 22px;
 }
-h3{
+
+h3 {
     font-family: 맑은고딕, Malgun Gothic, dotum, gulim, sans-serif;
     font-size: 18px;
 }
-p, label, span, a{
+
+p,
+label,
+span,
+a {
     font-family: 맑은고딕, Malgun Gothic, dotum, gulim, sans-serif;
     font-size: 16px;
 }
+
 .top {
     margin-left: 70px;
 }
+
 .container {
     display: flex;
 }
@@ -176,9 +213,8 @@ p, label, span, a{
 }
 
 .left>div {
-   border-bottom: 1px solid rgb(197, 195, 195);
+    border-bottom: 1px solid rgb(197, 195, 195);
 }
-
 
 .right {
     display: inline-block;
@@ -190,13 +226,13 @@ p, label, span, a{
 }
 
 .right>div {
-   border-bottom: 1px solid rgb(197, 195, 195);
+    border-bottom: 1px solid rgb(197, 195, 195);
 }
 
 .List {
     text-align: center;
-    padding-top:10px;
-    padding-bottom:10px;
+    padding-top: 10px;
+    padding-bottom: 10px;
 }
 
 .check {
@@ -212,17 +248,16 @@ p, label, span, a{
 
 .pList1 {
     text-align: center;
-       border-bottom: 1px solid black;
+    border-bottom: 1px solid black;
 }
-
 
 .content {
     padding: 10px;
 }
-.content>h3 {
- margin-left: 270px;
-}
 
+.content>h3 {
+    margin-left: 270px;
+}
 
 .mdText {
     width: 350px;
@@ -239,36 +274,35 @@ p, label, span, a{
     margin-left: 20px;
 }
 
-.logoutbtn{
+.logoutbtn {
     margin-top: 300px;
     margin-left: 35px;
 }
-.mod{
-    margin-left: 400px;
-    margin-right: 30px;
+.quit {
+    margin-left: 480px;
+    background-color: #00ba54;
 }
-.quit{
-   background-color: #00ba54;
-}
-
-#m1{
-  margin-left: 47px;
-}
-#m2{
-    margin-left: 28px;
-}
-#m3{
+#m1 {
     margin-left: 47px;
 }
-#m4{
+
+#m2 {
+    margin-left: 28px;
+}
+
+#m3 {
+    margin-left: 29px;
+}
+
+#m4 {
     margin-left: 65px;
 }
-#m5{
+
+#m5 {
     margin-left: 45px;
     width: 450px;
 }
-.pwFlag{
+.pwFlag, .numFlag {
     color: red;
 }
-
 </style>
